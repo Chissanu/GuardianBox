1840f973b343cc9669dda37dbd1568d7ed90d952
FiresoftPH <most.chuti@gmail.com>
2024-03-07 17:24:22 +0700

    Added Nextcloud functionalities and fixed some encoding bugs

--- server.py
+++ server.py
@@ -2,14 +2,22 @@ from flask import Flask, Response
 import cv2
 import time
 import numpy as np
+import os
+import nextcloud_client
+from dotenv import dotenv_values
 
 app = Flask(__name__)
+config = dotenv_values(".env")
+
+nc = nextcloud_client.Client("http://172.20.10.4/nextcloud")
+nc.login(config['NC_USER'], config['NC_PASS'])
 
 # This function will always run and will stream the camera output alongside taking videos
 def camera_feed(camera_index):
     video_index = 0
     """Video streaming generator function."""
     vs = cv2.VideoCapture(camera_index)
+    vs.set(cv2.CAP_PROP_FPS, 10)
     start_time = time.time()
     video_frames = []
     while True:
@@ -29,10 +37,14 @@ def camera_feed(camera_index):
                 out.write(frame)
             video_frames = []
             start_time = time.time()
-            if video_index > 10:
+            if video_index >= 5:
                 video_index = 0
             else:
                 video_index += 1
+            
+            nc_dir = "GuardianBox/cctv/" + output_file
+            nc.put_file(nc_dir, output_file)
+            print("Video Wrote")
             out.release()
 
     vs.release()
