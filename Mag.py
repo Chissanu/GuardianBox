3896857e3313b0a137811530ea428aacd272b198
785putt <mr.putty02@gmail.com>
2024-03-28 14:27:56 +0700

    working magnetic lock

--- Mag.py
+++ Mag.py
@@ -5,12 +5,11 @@ from hx711 import HX711
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
 # GPIO.cleanup()
 
 broker = "broker.hivemq.com"
 port = 1883
-
 hx = HX711(dout_pin=6, pd_sck_pin=5)
 
 
@@ -18,10 +17,10 @@ def on_message(client, usrdata, message):
     print("Receive : " + str(message.payload.decode()) + "\n")
     if(message.payload.decode() == "0"):
         client.publish("Magna/LampSta", "0")
-        GPIO.output(18, False)
+        GPIO.output(21, False)
     else:
         client.publish("Magna/LampSta", "1")
-        GPIO.output(18, True)
+        GPIO.output(21, True)
         # reading = hx.get_raw_data()
         # print(reading[0])
         # client.publish("Load", str(reading[0]))
@@ -34,13 +33,12 @@ def on_connect(client, usrdata, flags, rc):
         
 
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BCM)
 
 client = mqtt.Client()
 # client.username_pw_set(username="kmitliot",password="KMITL@iot1234")
 client.on_connect = on_connect
-
 client.on_message = on_message
 
 
@@ -51,17 +49,7 @@ client.subscribe("Magna/LampCmd", 0)
 while True:
     client.loop_start()
 
-    reading = hx.get_raw_data()
-    weight_reading = reading[0]     # Adjust the value output as desired (diving by 100 or so)
-
-    # Adjust the weight_reading value as needed (I set it 2250 for testing purpose solely, since the readings are inaccurate)
-    if weight_reading > 2250:
-        client.publish("Load", str(weight_reading))
-        print("Weight reading published successfully:", weight_reading)
-    else:
-        print("Weight reading is below 2250. Not publishing.")
-
-    time.sleep(0.1)
+    # time.sleep(0.1)
 
     # Stop MQTT client loop to ensure connection maintenance
     client.loop_stop()
