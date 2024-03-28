fa74a3e9adc335ed4912cec5e311ebb44f7b579f
785putt <mr.putty02@gmail.com>
2024-03-28 16:04:10 +0700

    ultra-sensor with node red

--- Mag.py
+++ Mag.py
@@ -1,7 +1,11 @@
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import time
-from hx711 import HX711
+from hcsr04sensor import sensor
+
+# set gpio pins
+PIN = 17
+echo = 27
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -10,8 +14,7 @@ GPIO.setup(21, GPIO.OUT)
 
 broker = "broker.hivemq.com"
 port = 1883
-hx = HX711(dout_pin=6, pd_sck_pin=5)
-
+x = sensor.Measurement
 
 def on_message(client, usrdata, message):
     print("Receive : " + str(message.payload.decode()) + "\n")
@@ -49,7 +52,17 @@ client.subscribe("Magna/LampCmd", 0)
 while True:
     client.loop_start()
 
-    # time.sleep(0.1)
+    distance = round(x.basic_distance(PIN, echo),2)
+
+    if distance <= 28.4:
+        client.publish("Load", str(distance))
+        print("Item detected")
+
+    print("The distance at  20 Celsius is {} cm's".format(distance))
+    # cleanup gpio pins.
+    GPIO.cleanup((PIN, echo))
+
+    time.sleep(1)
 
     # Stop MQTT client loop to ensure connection maintenance
     client.loop_stop()
