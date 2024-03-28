e19bf628068c9f3b8041a045391ef7b2a59a4f76
Chissanu <chissanuoak2@gmail.com>
2024-03-28 17:39:20 +0700

    Added Ultrasonic sensor testing code

--- Controller.py
+++ Controller.py
@@ -1,8 +1,13 @@
 import RPi.GPIO as GPIO
+import time
+from hcsr04sensor import sensor
 
 class Contoller():
-    def __init__(self, led_pin):   
+    def __init__(self, led_pin, ultra_pin, echo_pin):   
         self.led_pin = led_pin
+        self.ultra_pin = ultra_pin
+        self.echo_pin = echo_pin
+
         GPIO.setwarnings(False)
         GPIO.cleanup()
         GPIO.setmode(GPIO.BCM)
@@ -13,3 +18,11 @@ class Contoller():
 
     def turn_off(self):
         GPIO.output(self.led_pin, False)
+    
+    def read_ultrasonic(self):
+        x = sensor.Measurement
+        distance = x.basic_distance(self.ultra_pin, self.echo_pin)
+                
+        return round(distance,2)
+
+    
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import psycopg2
-import random
+import time
+import paho.mqtt.client as mqtt
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from datetime import datetime
 from Controller import Contoller
 
+
 DATABASE = "access_log_database"
 DEFAULT_DB = "postgres"
 DATABASE_USER = "pi"
 DATABASE_PASS = "Chissanu1"
+BROKER = "broker.hivemq.com"
+PORT = 1883
 
 def get_db_connection():
     conn = psycopg2.connect(
@@ -43,7 +47,6 @@ def create_log_table():
     command = ("""
         CREATE TABLE logs (
             id SERIAL PRIMARY KEY,
-            weight FLOAT DEFAULT 0.0,
             time_open TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             time_close TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
@@ -74,28 +77,73 @@ def openCrate():
         crateOpen = input("Still open? t/f >")
     timeclose_val = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    weight = getWeight()
 
-    command = f"INSERT INTO logs (weight,time_open, time_close) values ('{weight}','{timestamp_val}','{timeclose_val}')"
+    command = f"INSERT INTO logs (time_open, time_close) values ('{timestamp_val}','{timeclose_val}')"
     insert_to_database(command)
 
-def getWeight():
-    rand_weight = random.randint(1,50)
-    # Psudo weight
-    return rand_weight
+def check_if_item_inside():
+    print("===Door unlocked===")
+    doorOpen = True
+    while doorOpen:
+        distance = controller.read_ultrasonic()
+        print(distance)
+        if distance <= 28.4:
+            print("Occupied")
+            # print("====Door locked===")
+            client.publish("GuardianBox/sonic-1-status", "Occupied")
+        else:
+            print("vacant")
+            client.publish("GuardianBox/sonic-1-status", "Vacant")
+        
+        # time.sleep(0.25)
+    
 
-controller = Contoller(4)
+
+light_led = 4
+ultra_pin = 17
+echo_pin = 27
+
+controller = Contoller(light_led,ultra_pin,echo_pin)
+
+def on_message(client, usrdata, message):
+    print("Receive : " + str(message.payload.decode()) + "\n")
+    if(message.payload.decode() == "0"):
+        client.publish("GuardianBox/SonicSta", "0")
+    else:
+        client.publish("GuardianBox/SonicSta", "1")
+
+
+def on_connect(client, usrdata, flags, rc):
+    if rc == 0:
+        print("Successfully Connected")
+    else:
+        print("Error")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+
+client.connect(BROKER, PORT)
+client.subscribe("GuardianBox/SonicCmd", 0)
 
 opt = None
 while opt != 0:
-    if opt == 1:
-        create_database()
-    elif opt == 2:
-        create_log_table()
-    elif opt == 3:
-        openCrate()
-    elif opt == 4:
-        controller.turn_on()
-    elif opt == 5:
-        controller.turn_off()
-    opt = int(input("Menu: \n0.Exit\n1.Create DB \n2.Create Table\n3.Open Crate \n4.Turn on light \n5.Turn off light \n> "))
+    client.loop_start()
+    check_if_item_inside()
+    # client.publish("GuardianBox/SonicSta", "hehe")
+    # if opt == 1:
+    #     create_database()
+    # elif opt == 2:
+    #     create_log_table()
+    # elif opt == 3:
+    #     openCrate()
+    # elif opt == 4:
+    #     controller.turn_on()
+    # elif opt == 5:
+    #     controller.turn_off()
+    # elif opt == 6:
+    #     check_if_item_inside()
+    client.loop_stop()
+    # opt = int(input("Menu: \n0.Exit\n1.Create DB \n2.Create Table\n3.Open Crate \n4.Turn on light \n5.Turn off light \n> "))
+# client.loop_forever()
